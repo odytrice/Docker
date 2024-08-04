ba933ebbb5c92cb77953b2aca01ba599dc5a18bb
Ody Mbegbu <odytrice@gmail.com>
2024-08-03 22:31:53 -0700

    Fixed Runner

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ docker push --all-tags odytrice/cassandra
 docker build -t odytrice/kafka:3.7.0 -t odytrice/kafka:latest kafka
 docker push --all-tags odytrice/kafka
 
-docker build -t odytrice/github-runner:2.318.0 -t odytrice/github-runner:latest github-runner
+docker build -t odytrice/github-runner:2.317.0 -t odytrice/github-runner:latest github-runner
 docker push --all-tags odytrice/github-runner
--- github-runner/startup.sh
+++ github-runner/startup.sh
@@ -14,8 +14,31 @@ fi
 
 # Get the hostname
 HOSTNAME=$(hostname)
-
 RUNNER_NAME=${RUNNER_NAME:-$HOSTNAME}
+WORKDIR=${WORKDIR:-/home/runner/work}
 
-./config.sh --name $RUNNER_NAME --url $REPO_URL --token $RUNNER_TOKEN --work ./work
-./run.sh --name $RUNNER_NAME
+# Add Docker Permissions
+DOCKER_SOCKET=/var/run/docker.sock
+RUNUSER=runner
+
+if [ -S ${DOCKER_SOCKET} ]; then
+    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+    DOCKER_GROUP=$(getent group ${DOCKER_GID} | awk -F ":" '{ print $1 }')
+    if [ $DOCKER_GROUP ]
+    then
+        usermod -aG $DOCKER_GROUP $RUNUSER
+    else
+        groupadd -g ${DOCKER_GID} docker-admin
+        usermod -aG docker-admin $RUNUSER
+    fi
+fi
+
+
+# Create Working Directory
+mkdir -p $WORKDIR
+chown -R ${RUNUSER} $WORKDIR
+
+# tail -f /dev/null
+
+su $RUNUSER -c "./config.sh --unattended --url $REPO_URL --token $RUNNER_TOKEN --work $WORKDIR"
+su $RUNUSER -c "./run.sh"
