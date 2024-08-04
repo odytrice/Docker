9c14c78b2d37b2d39374476e64b793fec2c24800
Ody Mbegbu <odytrice@gmail.com>
2024-08-04 00:42:35 -0700

    Added Kustomize and Kubectl

--- github-runner/startup.sh
+++ github-runner/startup.sh
@@ -18,27 +18,34 @@ RUNNER_NAME=${RUNNER_NAME:-$HOSTNAME}
 WORKDIR=${WORKDIR:-/home/runner/work}
 
 # Add Docker Permissions
+echo "Setting Docker Permissions"
 DOCKER_SOCKET=/var/run/docker.sock
+DOCKER_GID=2375
 RUNUSER=runner
-
 if [ -S ${DOCKER_SOCKET} ]; then
     DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
-    DOCKER_GROUP=$(getent group ${DOCKER_GID} | awk -F ":" '{ print $1 }')
-    if [ $DOCKER_GROUP ]
-    then
-        usermod -aG $DOCKER_GROUP $RUNUSER
-    else
-        groupadd -g ${DOCKER_GID} docker-admin
-        usermod -aG docker-admin $RUNUSER
-    fi
 fi
 
+DOCKER_GROUP=$(getent group ${DOCKER_GID} | awk -F ":" '{ print $1 }')
+if [ $DOCKER_GROUP ]
+then
+    echo "Adding User to Existing Docker Group"
+    usermod -aG $DOCKER_GROUP $RUNUSER
+    newgrp $DOCKER_GROUP
+else
+    echo "Creating Group and User based on docker socket uid and gid"
+    groupadd -g ${DOCKER_GID} dind
+    usermod -aG dind $RUNUSER
+    newgrp dind
+fi
 
 # Create Working Directory
+echo "Create/Change Owner for Work Directory"
 mkdir -p $WORKDIR
 chown -R ${RUNUSER} $WORKDIR
 
 # tail -f /dev/null
+# su runner -c "docker ps"
 
 su $RUNUSER -c "./config.sh --unattended --url $REPO_URL --token $RUNNER_TOKEN --work $WORKDIR"
 su $RUNUSER -c "./run.sh"
